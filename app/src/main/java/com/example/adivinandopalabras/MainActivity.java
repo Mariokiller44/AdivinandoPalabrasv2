e078471f0f78e45bd5b70d9bd262048163a0a2f3
Mariokiller44 <84866133+Mariokiller44@users.noreply.github.com>
2022-11-07 10:02:31 +0100

    examen Adivinar Palabra (4 nov 2022)

--- app/src/main/java/com/example/adivinandopalabras/MainActivity.java
+++ app/src/main/java/com/example/adivinandopalabras/MainActivity.java
@@ -1,40 +1,25 @@
 package com.example.adivinandopalabras;
 
-import static android.widget.TextView.BufferType.NORMAL;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
- /*   public String[] palabras = {"Memoria", "Inteligencia", "Capacidad"};
-    int intentos = 5;
-    int posi = (int) Math.floor(Math.random() * palabras.length);
-    String palabraNueva = palabras[posi];
-    char[] caracteres = palabraNueva.toCharArray();
-    public char guiones[] = new char[caracteres.length];
-    public boolean[] comprobar = new boolean[caracteres.length];
-    String nuevaP;
-    TextView palabra;
-*/
     Partida p = new Partida();
     TextView mostrarPalabra;
     TextView mostrarIntentos;
@@ -44,9 +29,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-  //      mostrarJuego();
           mostrarIntentos();
           mostrarPalabra();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            //Ejercicio 1
+            case R.id.verPalabra:
+                String palabraEscogida = p.getPalabraSeleccionada();
+                Toast.makeText(this,"La palabra es: "+palabraEscogida,Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.aniadirNuevaPalabra:
+                ArrayList<String> palabras = p.getPalabras();
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Nueva Palabra")
+                        .setMessage("Introduzca nueva palabra");
+                final EditText palabra = new EditText(MainActivity.this);
+                LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                palabra.setLayoutParams(layout);
+                builder.setView(palabra);
+                builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        palabras.add(palabra.getText().toString());
+                        p.setPalabras(palabras);
+                        Toast.makeText(getApplicationContext(),"Correcta insercion",Toast.LENGTH_SHORT+1).show();
+                    }
+                });
+                builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create();
+                builder.show();
+                return true;
+            case R.id.MuestraListaPalabras:
+                Intent i = new Intent(this, mostrarPalabras.class);
+                ArrayList<String> listaPalabras = p.getPalabras();
+                i.putStringArrayListExtra("listaPalabras",listaPalabras);
+                startActivity(i);
+                return true;
+                //Ejercicio 4
+            case R.id.salir:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void mostrarIntentos(){
@@ -142,131 +183,4 @@ public class MainActivity extends AppCompatActivity {
           mostrarPalabra();
     }
 
-
-/*    public void mostrarJuego() {
-        mostrarIntentos();
-        mostrarNuevaPalabra();
-    }
-
-    public void cambiarPalabra(View v){
-            Button boton = (Button) findViewById(R.id.nuevo);
-            boton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    cambiarPalabraNueva();
-                }
-            });
-    }
-    public void cambiarPalabraNueva(){
-                intentos=5;
-                int nuevaWord= (int) Math.floor(Math.random()*palabras.length);
-                palabraNueva= palabras[nuevaWord];
-                caracteres=new char[]{};
-                caracteres=palabraNueva.toCharArray();
-                comprobar=new boolean[caracteres.length];
-                mostrarIntentos();
-                mostrarNuevaPalabra();
-    }
-
-
-    public void mostrarNuevaPalabra() {
-        palabra = (TextView) findViewById(R.id.palabra);
-        nuevaP = "";
-        if (comprobar.toString().isEmpty()){
-            cargaBuleanos();
-        }else {
-            for (int i = 0; i < palabraNueva.length(); i++) {
-                if (comprobar[i] == false) {
-                    char guion = '_';
-                    nuevaP = nuevaP.concat(guion + " ");
-                } else {
-                    nuevaP = nuevaP.concat(caracteres[i] + " ");
-                }
-            }
-            if (nuevaP.contains(String.valueOf('_'))==false){
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Enhorabuena");
-                builder.setMessage("Has acertado la palabra");
-                builder.setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.create();
-                builder.show();
-            }
-        }
-        palabra.setText(nuevaP);
-    }
-
-    public void cargaBuleanos() {
-        for (int i=0;i<comprobar.length;i++){
-            comprobar[i]=false;
-        }
-    }
-
-    public void mostrarIntentos () {
-        TextView textoIntentos = (TextView) findViewById(R.id.intentos);
-        textoIntentos.setText("Intentos: " + intentos);
-    }
-    public void mostrarCaracter(View v) {
-        TextView letraRecoger = (TextView) findViewById(R.id.letra);
-        if (letraRecoger.length() == 0){
-            Toast.makeText(this,"Letra por favor",Toast.LENGTH_SHORT).show();
-        }else {
-            char letra = Character.toUpperCase(letraRecoger.getText().charAt(0));
-            if (existe(letra, palabraNueva) != false) {
-                for (int i = 0; i < caracteres.length; i++) {
-                    if (Character.toUpperCase(caracteres[i]) == letra) {
-                        comprobar[i] = true;
-                    }
-                }
-                letraRecoger.setText("");
-                mostrarNuevaPalabra();
-            } else {
-                letraRecoger.setText("");
-                intentos--;
-                mostrarIntentos();
-            }
-            if (intentos == 0) {
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                builder1.setTitle("Vuelve a intentarlo");
-                builder1.setMessage("No has acertado la palabra; la palabra era " + palabraNueva);
-                builder1.setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        intentos = 5;
-                        cambiarPalabraNueva();
-                        mostrarJuego();
-                        dialog.dismiss();
-                    }
-                });
-                builder1.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder1.create();
-                builder1.show();
-            }
-        }
-    }
-
-    private boolean existe(char letra, String palabraNueva) {
-        boolean salida=false;
-        if (palabraNueva.contains(String.valueOf(letra))){
-            salida=true;
-        }else if(palabraNueva.contains(String.valueOf(Character.toLowerCase(letra)))){
-            salida=true;
-        }
-        return salida;
-    }*/
 }
